dd9943bd987a888d7cd59e150d83f7829d56d0f9
Vitor Guimarães <vitorrawwr@gmail.com>
2025-05-14 18:14:36 +0100

    tentativa de resolver o problema de CSP

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,32 @@
-// webpack.config.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  // 1. Ponto de entrada
-  entry: './src/components/App.js',
-
-  // 2. Onde vai sair o bundle
+  entry: './src/index.js', 
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,            // limpa dist antes de cada build
+    clean: true, 
   },
-
-  // 3. Loaders para JS e CSS
   module: {
     rules: [
       {
-        test: /\.js$/,      // todos os arquivos .js
-        exclude: /node_modules/,
-        use: 'babel-loader' // precisa do babel-loader instalado
-      },
-      {
-        test: /\.css$/,     // para importar CSS dentro do JS (opcional)
-        use: ['style-loader','css-loader']
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader'],
       }
-    ]
+    ],
   },
-
-  // 4. Plugins
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body'
-    })
+      template: './src/index.html', 
+      filename: 'index.html'        
+    }),
   ],
-
-  // 5. Modo e servidor de dev
-  mode: 'development',
   devServer: {
-    static: './dist',
-    hot: true,
-    open: true
-  }
+    static: './dist', 
+    open: true,      
+    port: 8080,
+  },
+  devtool: 'source-map', 
+  mode: 'development',
 };
